565da1d83759a9ad49dd73f4263d924df810c19d
OriLev <sviatos@gmail.com>
2016-12-04 17:24:48 +0200

    pre connecting to DB

---
+++ BeerModel.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var beerSchema = new Schema({
+	name: String,
+	style: String,
+	image_url: String,
+	abv: Number
+})
+
+var Beer = mongoose.model("Beer", beerSchema);
+module.exports = Beer;
